4b7ceb0ca72dbdcdff7c7239477bffbd3cd2070b
unknown <pemeraldy@gmail.com>
2020-07-31 16:45:37 +0100

    Nav cart items display

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,6 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
+import Store from '../views/Store.vue'
 import Checkout from '../views/Checkout.vue'
 import Message from '../views/Message.vue'
 import ProductDetails from '../views/ProductDetails.vue'
@@ -10,8 +10,8 @@ Vue.use(VueRouter)
 const routes = [
   {
     path: '/',
-    name: 'Home',
-    component: Home
+    name: 'Store',
+    component: Store
   },
   {
     path: '/checkout',
---
+++ src/views/Store.vue
@@ -0,0 +1,47 @@
+<template>
+  <div class="container-fluid">
+    <div class="row">
+      <div class="col-md-12">
+        <div class="filterbar">
+          <h3 class="text-center">Filter Bar</h3>
+        </div>
+      </div>
+    </div>
+    <div class="row">
+      <div class="col-md-3">
+        <div class="card text-white bg-primary mb-3" style="max-width: 20rem;">
+          <div class="card-header">
+            <h4 class="text-white text-center">Categories</h4>
+          </div>
+          <div class="card-body">
+            <div class="list-group">
+              <a href="#" class="bg-dark text-white list-group-item list-group-item-action">
+                <i class="fas fa-air-freshener"></i> Latest
+              </a>
+              <a href="#" class="bg-dark text-white list-group-item list-group-item-action">
+                <i class="fa fa-child" aria-hidden="true"></i> Children
+              </a>
+              <a href="#" class="bg-dark text-white list-group-item list-group-item-action">Smart</a>
+
+              <a href="#" class="bg-dark text-white list-group-item list-group-item-action">Digital</a>
+
+              <a href="#" class="bg-dark text-white list-group-item list-group-item-action">Analogue</a>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="col-md-9"></div>
+    </div>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+// import Navbar from "@/components/Navbar";
+export default {
+  name: "Home",
+  components: {
+    // Navbar,
+  },
+};
+</script>
